d712671a1671fbde4a58975ddbe837d03c58b8c9
Konrad T. A. ALAHASSA <am.konrad21@gmail.com>
2024-05-27 20:46:52 -0400

    Add env config

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-
-const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
-};
-
+import {firebaseConfig} from "../utils/configs";
 
 const app = initializeApp(firebaseConfig);
 
