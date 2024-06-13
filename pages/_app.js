461576728f1c1ef03c0dc693b821060869c7a7af
Konrad T. A. ALAHASSA <am.konrad21@gmail.com>
2024-06-12 22:52:00 -0400

    update ui lib to nextui

--- pages/_app.js
+++ pages/_app.js
@@ -8,56 +8,71 @@ import AuthProvider from '../firebase/Context/AuthContext';
 import ProtectedRoute from '../Components/ProtectedRoute/ProtectedRoute';
 import AlreadyLogin from '../Components/AlreadyLogin/AlreadyLogin';
 import { StateContextProvider } from '../Context/StateContext';
+import {NextUIProvider} from "@nextui-org/react";
 
+
+// function MyApp({ Component, pageProps }) {
+//   switch (Component) {
+//     case Home: {
+//       return (
+//         <AuthProvider>
+//           <StateContextProvider>
+//             <AlreadyLogin>
+//               <TopLayout>
+//                 <Component {...pageProps} />
+//               </TopLayout>
+//             </AlreadyLogin>
+//           </StateContextProvider>
+//         </AuthProvider>
+//       );
+//     }
+//     case (Login):
+//       return (
+//         <AuthProvider>
+//           <StateContextProvider>
+//             <AlreadyLogin>
+//               <Component {...pageProps} />
+//             </AlreadyLogin>
+//           </StateContextProvider>
+//         </AuthProvider>
+//       );
+//     case (SignUp):
+//       return (
+//         <AuthProvider>
+//           <StateContextProvider>
+//             <AlreadyLogin>
+//               <Component {...pageProps} />
+//             </AlreadyLogin>
+//           </StateContextProvider>
+//         </AuthProvider>
+//       );
+//     default:
+//       return (
+//         <AuthProvider>
+//           <StateContextProvider>
+//             <ProtectedRoute>
+//               <SideLayout>
+//                 <Component {...pageProps} />
+//               </SideLayout>
+//             </ProtectedRoute>
+//           </StateContextProvider>
+//         </AuthProvider>
+//       )
+//   }
+// }
 
 function MyApp({ Component, pageProps }) {
-  switch (Component) {
-    case Home: {
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <AlreadyLogin>
-              <TopLayout>
+  return (
+      <NextUIProvider>
+          <AuthProvider>
+            <StateContextProvider>
+              {/*<AlreadyLogin>*/}
                 <Component {...pageProps} />
-              </TopLayout>
-            </AlreadyLogin>
-          </StateContextProvider>
-        </AuthProvider>
-      );
-    }
-    case (Login):
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <AlreadyLogin>
-              <Component {...pageProps} />
-            </AlreadyLogin>
-          </StateContextProvider>
-        </AuthProvider>
-      );
-    case (SignUp):
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <AlreadyLogin>
-              <Component {...pageProps} />
-            </AlreadyLogin>
-          </StateContextProvider>
-        </AuthProvider>
-      );
-    default:
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <ProtectedRoute>
-              <SideLayout>
-                <Component {...pageProps} />
-              </SideLayout>
-            </ProtectedRoute>
-          </StateContextProvider>
-        </AuthProvider>
-      )
-  }
+              {/*</AlreadyLogin>*/}
+            </StateContextProvider>
+          </AuthProvider>
+      </NextUIProvider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -4,63 +4,70 @@ import easyToManage from '../Images/easyToManage.webp';
 import userFriendly from '../Images/userFriendly.webp';
 import alwaysImproving from '../Images/alwaysImproving.webp';
 import Head from 'next/head';
+import Login from "./login";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <meta name="description" content="Easy to manage Stock" />
+//         <meta name="description" content="stock management system" />
+//         <meta name="description" content="user friendly stock management system" />
+//         <title>MedAssist.io</title>
+//       </Head>
+//       <div className={classes.main_container}>
+//         <div className={classes.sub_conatiner}>
+//           <div className={classes.content}>
+//             <h1>Easy to Manage Stock!</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, magni. Sed non hic deleniti, illo temporibus minima molestias facilis enim placeat. Dolor ducimus similique totam laudantium magnam eveniet quod atque pariatur. Eaque, consequuntur eligendi!</p>
+//           </div>
+//           <div className={classes.img}>
+//             <Image
+//               src={easyToManage}
+//               width={300}
+//               height={300}
+//               alt="Management"
+//               priority="performance"
+//             />
+//           </div>
+//         </div>
+//         <div className={classes.sub_conatiner_rev}>
+//           <div className={classes.content}>
+//             <h1>User Friendly Interface!</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, magni. Sed non hic deleniti, illo temporibus minima molestias facilis enim placeat. Dolor ducimus similique totam laudantium magnam eveniet quod atque pariatur. Eaque, consequuntur eligendi!</p>
+//           </div>
+//           <div className={classes.img}>
+//             <Image
+//               src={userFriendly}
+//               width={300}
+//               height={300}
+//               alt="Management"
+//               priority="performance"
+//             />
+//           </div>
+//         </div>
+//         <div className={classes.sub_conatiner}>
+//           <div className={classes.content}>
+//             <h1> always updating!</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, magni. Sed non hic deleniti, illo temporibus minima molestias facilis enim placeat. Dolor ducimus similique totam laudantium magnam eveniet quod atque pariatur. Eaque, consequuntur eligendi!</p>
+//           </div>
+//           <div className={classes.img}>
+//             <Image
+//               src={alwaysImproving}
+//               width={300}
+//               height={300}
+//               alt="Management"
+//               priority="performance"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <meta name="description" content="Easy to manage Stock" />
-        <meta name="description" content="stock management system" />
-        <meta name="description" content="user friendly stock management system" />
-        <title>MedAssist.io</title>
-      </Head>
-      <div className={classes.main_container}>
-        <div className={classes.sub_conatiner}>
-          <div className={classes.content}>
-            <h1>Easy to Manage Stock!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, magni. Sed non hic deleniti, illo temporibus minima molestias facilis enim placeat. Dolor ducimus similique totam laudantium magnam eveniet quod atque pariatur. Eaque, consequuntur eligendi!</p>
-          </div>
-          <div className={classes.img}>
-            <Image
-              src={easyToManage}
-              width={300}
-              height={300}
-              alt="Management"
-              priority="performance"
-            />
-          </div>
-        </div>
-        <div className={classes.sub_conatiner_rev}>
-          <div className={classes.content}>
-            <h1>User Friendly Interface!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, magni. Sed non hic deleniti, illo temporibus minima molestias facilis enim placeat. Dolor ducimus similique totam laudantium magnam eveniet quod atque pariatur. Eaque, consequuntur eligendi!</p>
-          </div>
-          <div className={classes.img}>
-            <Image
-              src={userFriendly}
-              width={300}
-              height={300}
-              alt="Management"
-              priority="performance"
-            />
-          </div>
-        </div>
-        <div className={classes.sub_conatiner}>
-          <div className={classes.content}>
-            <h1> always updating!</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, magni. Sed non hic deleniti, illo temporibus minima molestias facilis enim placeat. Dolor ducimus similique totam laudantium magnam eveniet quod atque pariatur. Eaque, consequuntur eligendi!</p>
-          </div>
-          <div className={classes.img}>
-            <Image
-              src={alwaysImproving}
-              width={300}
-              height={300}
-              alt="Management"
-              priority="performance"
-            />
-          </div>
-        </div>
-      </div>
-    </>
+      <Login/>
   )
 }
